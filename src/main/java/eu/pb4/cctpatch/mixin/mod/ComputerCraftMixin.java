b8d5c4b147af81751aa2fae00a32e39ec404f93c
Patbox <39821509+Patbox@users.noreply.github.com>
2024-08-12 18:04:09 +0200

    Fix crash on prod

--- src/main/java/eu/pb4/cctpatch/mixin/mod/ComputerCraftMixin.java
+++ src/main/java/eu/pb4/cctpatch/mixin/mod/ComputerCraftMixin.java
@@ -1,21 +1,22 @@
 package eu.pb4.cctpatch.mixin.mod;
 
+import dan200.computercraft.shared.CommonHooks;
 import dan200.computercraft.shared.ComputerCraft;
 import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Overwrite;
 
-import java.util.Optional;
 
 @Mixin(value = ComputerCraft.class, remap = false)
 public class ComputerCraftMixin {
-    @Redirect(method = "lambda$init$18", at = @At(value = "INVOKE", target = "Lnet/minecraft/registry/Registry;getKey(Ljava/lang/Object;)Ljava/util/Optional;"))
-    private static Optional<RegistryKey<ItemGroup>> replaceKey(Registry instance,  Object t) {
-        return Optional.of(PolymerItemGroupUtils.getKey((ItemGroup) t));
+    /**
+     * @author Patbox
+     * @reason I need to replace item group key getting logic.
+     */
+    @Overwrite(remap = false)
+    private static void lambda$init$18(ItemGroup group, FabricItemGroupEntries entries) {
+        CommonHooks.onBuildCreativeTab(PolymerItemGroupUtils.getKey(group), entries.getContext(), entries);
     }
 }
